bee17262bca4e2b824e51bbf772dd8a36457f07b
userName <userEmail>
2024-11-11 22:11:28 +0800

    完善轮播图

---
+++ js/lunbo.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const slides = document.querySelectorAll('.slides .slide');
+    const radios = document.querySelectorAll('input[name="slider"]');
+    let currentIndex = 0;
+    function autoPlay() {
+        currentIndex = (currentIndex + 1) % slides.length;
+        changeSlide(currentIndex);
+    }
+
+    function changeSlide(index) {
+        slides.forEach((slide, i) => {
+            slide.style.opacity = 0;
+            radios[i].checked = false;
+        });
+        slides[index].style.opacity = 1;
+        radios[index].checked = true;
+    }
+    setInterval(autoPlay, 5000);
+
+    document.querySelector('.slides .left-arrow').addEventListener('click', () => {
+        currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+        changeSlide(currentIndex);
+    });
+
+    document.querySelector('.slides .right-arrow').addEventListener('click', () => {
+        currentIndex = (currentIndex + 1) % slides.length;
+        changeSlide(currentIndex);
+    });
+});
